0c44bfec19c63aae950dc2e4d06f0b3009e770d1
santiago mojica <samojica08@gmail.com>
2024-09-26 23:32:15 -0500

    Colores a la card

--- Ecommerce/src/components/Header.jsx
+++ Ecommerce/src/components/Header.jsx
@@ -3,18 +3,18 @@ import Carrito from './Carrito.jsx'
 
 function Header() {
     return (
-        <header className='flex h-16 w-full flex-row-reverse items-center justify-between px-2 sm:px-4 md:flex-row'>
-            <h1 className='hidden font-cursive text-4xl text-primero hover:scale-105 hover:cursor-pointer md:flex'>Sadimi</h1>
+        <header className='flex h-16 w-full flex-row items-center justify-evenly px-2 sm:px-4'>
             <div className='flex h-10 items-center justify-center rounded-xl p-2 px-4 hover:bg-segundo md:hidden'>
                 <img className='h-7' src="hamburguesa.svg" alt="menu" />
             </div>
+            <h1 className='hidden p-2 px-4 text-center font-cursive text-4xl text-primero hover:scale-105 hover:cursor-pointer md:flex md:w-80'>Sadimi</h1>
             <div className='flex h-10 w-4/6 max-w-80 items-center rounded-xl bg-tercero p-1 pl-4 shadow-md sm:w-80'>
                 <input className="flex h-full w-5/6 bg-tercero font-semibold text-primero outline-none placeholder:text-primero" type="text" placeholder='Buscar' />
                 <span className='sm:2/6 flex h-full w-1/6 items-center justify-center rounded-xl hover:cursor-pointer hover:bg-segundo'>
                     <img className='h-6 w-4/5 hover:cursor-pointer hover:bg-segundo' src="lupa.svg" alt="Lupa" />
                 </span>
             </div>
-            <div className='flex h-10 items-center sm:gap-4'>
+            <div className='flex h-10 items-center justify-end sm:gap-4 md:w-80'>
                 <span className='relative flex rounded-xl p-2 px-4 hover:scale-105 hover:cursor-pointer hover:bg-segundo'>
                     <Carrito client:load color={"#4c7766"} />
                     <span className='absolute left-1/2 top-0 flex h-4 w-4 items-center justify-center rounded-[50%] bg-cuarto text-[75%] font-normal text-tercero'>3</span>
