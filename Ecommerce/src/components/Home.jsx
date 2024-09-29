8dba607bb816cc8a645e72a55f7c3818d27cb971
santiago mojica <samojica08@gmail.com>
2024-09-29 12:27:34 -0500

    Correccion colores, nombres, tamaño del article

--- Ecommerce/src/components/Home.jsx
+++ Ecommerce/src/components/Home.jsx
@@ -20,27 +20,30 @@ function Home() {
         fetchProducts();
     }, []);
 
+
     console.log(products)
     return (
         <main className='my-20 flex w-full min-w-96 flex-wrap items-center justify-center gap-4 px-2 sm:px-4'>
             {products.map(product => (
-                <article key={product._id} className="h-80 w-40 justify-center overflow-hidden rounded-xl border-2 bg-tercero p-3 shadow-lg hover:cursor-pointer md:h-96 md:w-60">
+                <article key={product._id} className="flex h-[28rem] w-40 flex-col justify-center overflow-hidden rounded-xl border-2 bg-tercero p-3 shadow-lg hover:cursor-pointer md:w-60">
                     <div className='flex h-36 w-full items-center justify-center md:h-52'>
-                        <img src={product.imagen} alt={product.nombre} width={200} className='aspect-square rounded-xl shadow-md' />
+                        <img src={product.imagen} alt={product.nombre} width={200} className='aspect-square rounded-xl mix-blend-multiply shadow-md' />
                     </div>
-                    <div className='mt-2 flex flex-col md:my-2'>
+                    <div className='mb-auto mt-2 flex max-h-36 min-h-24 flex-col md:my-2'>
                         <h6 className='text-[0.7rem] text-primero'>{product.marca}</h6>
-                        <h1 className='mb-4 whitespace-nowrap text-lg font-semibold text-quinto md:text-2xl'>{product.nombre}</h1>
-                        <div className='flex flex-row flex-nowrap gap-3'>
-                            {product.colores && product.colores.map((color, index) => (
-                                <svg className='hover:scale-125' key={index} width="20px" height="20px" viewBox="0 0 16 16" fill={color} xmlns="http://www.w3.org/2000/svg">
+                        <h1 className='text-md mb-2 text-quinto md:text-2xl'>{product.nombre}</h1>
+                    </div>
+                    <div className='mt-auto flex flex-row flex-nowrap gap-2'>
+                        {product.colores && product.colores.map((color, index) => (
+                            <span className='flex items-center justify-center rounded-[50%] border border-primero p-1'>
+                                <svg className='hover:scale-125' key={index} width="16px" height="16px" viewBox="0 0 16 16" fill={color} stroke-width="2" xmlns="http://www.w3.org/2000/svg">
                                     <circle cx="8" cy="8" r="8" />
                                 </svg>
-                            ))}
+                            </span>
+                        ))}
 
-                        </div>
                     </div>
-                    <div className='flex h-14 flex-row items-center justify-between'>
+                    <div className='mt-auto flex h-14 flex-row items-center justify-between'>
                         <span className='flex flex-col'>
                             <h3 className='text-xl font-bold text-cuarto'>$ {product.precio_oferta}</h3>
                             <h4 className='text-md font-semibold text-quinto'><s>$ {product.valor}</s></h4>
