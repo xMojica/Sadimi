7719bdb3bdf8997cb21661588fb38d89aec74381
santiago mojica <samojica08@gmail.com>
2024-10-02 10:57:46 -0500

    Border

--- Sadimi/src/components/Home/Login/Registrarse.jsx
+++ Sadimi/src/components/Home/Login/Registrarse.jsx
@@ -7,7 +7,7 @@ function Registrarse() {
 
     return (
         <div id='register' className='fixed left-0 top-0 z-50 hidden h-full w-full items-center justify-center border border-quinto/15 bg-black/40 shadow-md backdrop-blur-sm'>
-            <div className='h-full w-full rounded-xl bg-tercero sm:w-1/2 md:h-3/4 md:w-3/4'>
+            <div className='h-full w-full bg-tercero sm:w-1/2 md:h-3/4 md:w-3/4 md:rounded-xl'>
                 <div className='flex w-full items-center justify-between p-5'>
                     <span className='h-8 w-8 md:h-10 md:w-10'>
                         <Back />
