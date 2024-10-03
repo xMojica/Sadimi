f8d4a9c4b7a3249492ab1cd22ddd501af7913f62
santiago mojica <samojica08@gmail.com>
2024-10-03 17:32:40 -0500

    Error al recuperar

--- Sadimi/src/components/Home/Login/Main/RecuperarContraseña.jsx
+++ Sadimi/src/components/Home/Login/Main/RecuperarContraseña.jsx
@@ -17,8 +17,8 @@ function RecuperarContraseña() {
 
     return (
         <section className='flex h-auto flex-col justify-center gap-6 rounded-xl bg-background p-10'>
-            <Alerta tipo={"Error"} mensaje={"Error al intentar recuperar el usuario"} />
-            <div className='flex h-12 items-center rounded-xl bg-tercero'>
+            <Alerta tipo={"Error"} mensaje={"No pudimos recuperar el usuario"} />
+            < div className='flex h-12 items-center rounded-xl bg-tercero'>
                 <img className='mx-4 h-6 w-6' src={Email} alt="Email" />
                 <span className='flex h-12 w-full flex-row items-center rounded-xl bg-tercero shadow-lg'>
                     <input className='bg-red h-full w-full rounded-r-xl border-l-8 border-background bg-tercero pl-3 text-lg text-primero outline-none placeholder:text-primero' type="email" placeholder='Email: ' onChange={handleChange} />
