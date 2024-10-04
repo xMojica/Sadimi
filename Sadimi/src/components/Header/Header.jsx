6bb5d1dbd873c90ca6a51ac452fd7b804122c4b7
santiago mojica <samojica08@gmail.com>
2024-10-04 10:25:42 -0500

    Correccion carrito

--- Sadimi/src/components/Header/Header.jsx
+++ Sadimi/src/components/Header/Header.jsx
@@ -7,6 +7,7 @@ import { Context } from '../../Context/main';
 function Header() {
     const context = useContext(Context);
 
+
     function handleClick() {
         context.setMostrarLogin(true)
         document.body.style.overflow = "hidden";
@@ -19,19 +20,23 @@ function Header() {
                 <div className='flex h-10 items-center justify-center rounded-xl p-2 px-4 hover:cursor-pointer hover:bg-segundo md:hidden' onClick={handleClick}>
                     <img className='h-7' src={Hamburguesa} alt="menu" />
                 </div>
-                <h1 className='flex p-2 px-4 text-center font-cursive text-4xl font-bold text-primero hover:scale-105 hover:cursor-pointer md:flex md:w-80'>Sadimi</h1>
+                <h1 className='mx-auto flex p-2 px-4 text-center font-cursive text-4xl font-bold text-primero hover:scale-105 hover:cursor-pointer md:flex md:w-80'>Sadimi</h1>
                 <span className='hidden md:flex'><Buscador /></span>
                 <div className='flex h-10 items-center justify-end sm:gap-4 md:w-80 md:min-w-60'>
-                    <span className='relative flex rounded-xl p-2 px-4 text-primero hover:scale-105 hover:cursor-pointer hover:bg-segundo'>
+                    <span className='relative hidden rounded-xl p-2 px-4 text-primero hover:scale-105 hover:cursor-pointer hover:bg-segundo md:flex'>
                         <Carrito color={"#4c7766"} />
                         <span className='absolute left-1/2 top-0 flex h-4 w-4 items-center justify-center rounded-[50%] bg-cuarto text-[75%] font-normal text-tercero'>{context.contador}</span>
                     </span>
-                    <button className='hidden rounded-xl p-2 px-4 text-lg font-bold text-primero hover:scale-105 hover:bg-segundo md:flex' onClick={handleClick}>Iniciar sesion</button>
+                    <button className='flex rounded-xl p-2 px-4 text-lg font-bold text-primero hover:scale-105 hover:bg-segundo' onClick={handleClick}>Iniciar</button>
                 </div>
             </div>
 
-            <div className='mt-6 flex h-16 w-full justify-center px-4 md:hidden'>
+            <div className='mt-6 flex h-16 w-full justify-between gap-4 px-4 md:hidden'>
                 <Buscador />
+                <span className='relative flex h-10 items-baseline justify-center rounded-xl p-1 text-center text-primero hover:scale-105 hover:cursor-pointer hover:bg-segundo'>
+                    <Carrito color={"#4c7766"} />
+                    <span className='absolute left-1/2 top-0 flex h-4 w-4 -translate-x-1/2 items-center justify-center rounded-[50%] bg-cuarto text-[75%] font-normal text-tercero'>{context.contador}</span>
+                </span>
             </div>
         </header >
 
