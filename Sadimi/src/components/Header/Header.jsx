2451f3067b54b64204d781c4e0c33c747206a9a4
santiago mojica <samojica08@gmail.com>
2024-10-04 10:53:11 -0500

    Correccion px en header

--- Sadimi/src/components/Header/Header.jsx
+++ Sadimi/src/components/Header/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
     return (
 
         <header className='flex w-full min-w-96 flex-col'>
-            <div className='flex h-16 w-full flex-row items-center justify-between rounded-xl px-2 sm:px-4'>
+            <div className='flex h-16 w-full flex-row items-center justify-between rounded-xl px-2 md:px-4'>
                 <div className='flex h-10 items-center justify-center rounded-xl p-2 px-4 hover:cursor-pointer hover:bg-segundo md:hidden' onClick={handleClick}>
                     <img className='h-7' src={Hamburguesa} alt="menu" />
                 </div>
@@ -28,19 +28,20 @@ function Header() {
                         <Carrito color={"#4c7766"} />
                         <span className='absolute left-1/2 top-0 flex h-4 w-4 items-center justify-center rounded-[50%] bg-cuarto text-[75%] font-normal text-tercero'>{context.contador}</span>
                     </span>
-                    <div className='flex rounded-xl p-2 px-4 text-lg font-bold text-primero hover:scale-105 hover:bg-segundo' onClick={handleClick}>
+                    <div className='flex rounded-xl p-2 text-lg font-bold text-primero hover:scale-105 hover:bg-segundo' onClick={handleClick}>
                         <img src={user} alt="Usuario" />
                     </div>
                 </div>
             </div>
 
-            <div className='mt-6 flex h-16 w-full justify-between gap-4 px-4 md:hidden'>
+            <div className='mt-6 flex h-16 w-full justify-between gap-4 px-2 md:hidden'>
                 <Buscador />
                 <span className='relative flex h-10 items-baseline justify-center rounded-xl p-2 text-center text-primero hover:scale-105 hover:cursor-pointer hover:bg-segundo'>
                     <Carrito color={"#4c7766"} />
                     <span className='absolute left-1/2 top-0 flex h-4 w-4 -translate-x-1/2 items-center justify-center rounded-[50%] bg-cuarto text-[75%] font-normal text-tercero'>{context.contador}</span>
                 </span>
             </div>
+
         </header >
 
 
