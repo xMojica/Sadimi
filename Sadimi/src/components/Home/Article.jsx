b98c92a372b7aaa33b433390e1b3c46b98c13f37
santiago mojica <samojica08@gmail.com>
2024-10-13 00:52:02 -0500

    Unidades en movil

--- Sadimi/src/components/Home/Article.jsx
+++ Sadimi/src/components/Home/Article.jsx
@@ -14,7 +14,7 @@ function Article({ product }) {
 
 
     return (
-        <article className="flex h-96 w-40 flex-col justify-center overflow-hidden rounded-xl border-2 bg-tercero p-3 shadow-md hover:cursor-pointer hover:shadow-2xl sm:w-60 md:h-[480px]">
+        <article className="flex h-96 w-40 flex-col justify-center overflow-hidden rounded-xl border-2 bg-tercero p-3 shadow-md hover:cursor-pointer hover:shadow-2xl sm:h-[480px] sm:w-60">
             <div className='relative flex h-32 w-full items-center justify-center sm:h-52'>
                 <img src={product.imagen} alt={product.nombre} width={200} className='z-40 aspect-square w-52 rounded-xl mix-blend-multiply' />
                 <div className='absolute left-0 top-0 z-40 w-14 justify-center rounded-lg bg-cuarto px-1 text-center text-sm font-bold text-tercero'>-30%</div>
@@ -24,13 +24,13 @@ function Article({ product }) {
                 <h1 className='text-sm text-quinto md:text-xl'>{product.nombre}</h1>
             </div>
             <div className='mt-auto'>
-                <div class="flex gap-x-1">
-                    <span class="text-xl text-yellow-500">★</span>
-                    <span class="text-xl text-yellow-500">★</span>
-                    <span class="text-xl text-yellow-500">★</span>
-                    <span class="text-xl text-yellow-500">★</span>
-                    <span class="text-xl text-gray-300">★</span>
-                    <h6 className='text-quinto'>(+54)</h6>
+                <div className="flex items-center gap-x-1">
+                    {[...Array(5)].map((_, i) => (
+                        <span key={i} className={`text-xl ${i < 4 ? 'text-yellow-500' : 'text-gray-300'}`}>
+                            {i < 4 ? '★' : '☆'}
+                        </span>
+                    ))}
+                    <h6 className='hidden text-[12px] text-quinto md:flex'>4.3 (54)</h6>
                 </div>
             </div>
 
@@ -43,7 +43,7 @@ function Article({ product }) {
                     </span>
                 ))}
             </div>
-            <div className='my-1 text-sm text-quinto'>
+            <div className='my-1 text-[11px] text-quinto md:text-sm'>
                 {product.cantidad > 15 ? "+15 unidades disponibles" : product.cantidad + " unidades disponibles"}
             </div>
 
