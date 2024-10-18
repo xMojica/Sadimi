2b0d8470d3aabed9f298895002f86f5ee24de04b
santiago mojica <samojica08@gmail.com>
2024-10-18 13:30:28 -0500

    Valores y total

--- Sadimi/src/components/Carrito/Cart.jsx
+++ Sadimi/src/components/Carrito/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useCallback } from 'react';
+import React, { useContext, useCallback, useState, useEffect } from 'react';
 import { Context } from '../../Context/main';
 import Header from "../Header/Header"
 import Footer from '../Footer/Footer';
@@ -10,11 +10,39 @@ import { useNavigate } from 'react-router-dom';
 function Cart() {
     const { cart, setCart, carrito, setCarrito } = useContext(Context);
     const navigate = useNavigate()
+    const [subtotal, setSubtotal] = useState(0)
+    const [envio, setEnvio] = useState(0)
+    const [total, setTotal] = useState(0)
     const vaciarCarrito = useCallback(() => setCarrito([]), [setCarrito]);
     const eliminarProducto = (product) => {
         console.log(product)
         setCarrito(carrito.filter(producto => producto._id !== product._id))
     }
+
+    const manejarCantidad = (product, cambio) => {
+        const nuevaCantidad = product.stock + cambio;
+        if (nuevaCantidad >= 0 && nuevaCantidad <= product.cantidad) {
+            setCarrito(prevCarrito => {
+                return prevCarrito.map(item =>
+                    item._id === product._id ? { ...item, stock: nuevaCantidad } : item
+                );
+            });
+            console.log(carrito)
+        };
+    }
+
+    useEffect(() => {
+        const calcularTotal = () => {
+            for (const product of carrito) {
+                setSubtotal(Math.floor(product.precio_oferta * product.stock));
+                parseInt(setTotal(Math.floor((product.precio_oferta * product.stock) + envio)));
+
+            }
+
+        }
+        calcularTotal();
+    }, [carrito])
+
 
     return (
         <>
@@ -32,9 +60,9 @@ function Cart() {
                                 </div>
                                 <div className='ml-auto mr-8 flex items-center gap-8'>
                                     <span className='flex max-w-max items-center gap-x-2 rounded-lg border-2 border-gray-300 p-2 text-sm shadow-lg lg:gap-x-4 lg:text-xl'>
-                                        <button className='rounded-lg px-2'>-</button>
+                                        <button className='rounded-lg px-2' onClick={() => { manejarCantidad(product, -1) }}>-</button>
                                         <p className=''>{product.stock}</p>
-                                        <button className='rounded-lg px-2'>+</button>
+                                        <button className='rounded-lg px-2' onClick={() => { manejarCantidad(product, 1) }}>+</button>
                                     </span>
                                     <button className='absolute right-0 top-0 rounded-[50%] p-1 text-tercero sm:p-2 lg:p-4' onClick={() => { eliminarProducto(product) }}>
                                         <img src={Cerrar} alt="cerrar" className='h-4 w-4 lg:h-6 lg:w-6' />
@@ -49,9 +77,9 @@ function Cart() {
                     <h1 className='items-center text-sm font-bold lg:text-xl'>Checkout</h1>
                     <hr />
                     <div className='inline-block items-center'>
-                        <h4 className='text-sm lg:text-lg'>Subtotal: <span className='float-end font-normal'>$3.999.900</span></h4>
+                        <h4 className='text-sm lg:text-lg'>Subtotal: <span className='float-end font-normal'>$ {subtotal}</span></h4>
                         <h4 className='text-sm lg:text-lg'>Envio: <span className='float-end font-normal text-primero'>Gratis</span></h4>
-                        <h4 className='mt-4 text-sm font-extrabold lg:text-lg'>Total: <span className='float-end font-normal'>$3.999.900</span></h4>
+                        <h4 className='mt-4 text-sm font-extrabold lg:text-lg'>Total: <span className='float-end font-normal'>$ {total}</span></h4>
                     </div>
                     <button className='rounded-xl bg-primero p-2 text-xl text-tercero hover:scale-105'>Pagar</button>
                 </section>
