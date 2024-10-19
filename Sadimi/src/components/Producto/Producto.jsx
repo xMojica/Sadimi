475ae6194283941190bf086d42a89d91f8464e7a
santiago mojica <samojica08@gmail.com>
2024-10-19 13:12:08 -0500

    Product

--- Sadimi/src/components/Producto/Producto.jsx
+++ Sadimi/src/components/Producto/Producto.jsx
@@ -10,11 +10,11 @@ function Producto() {
         <>
             <Header />
             <main className='mt-36 px-4 sm:mt-32'>
-                <div className='flex-wrap flex items-start min-w-min justify-center  bg-slate-300 rounded-xl p-4'>
-                    <section className='flex max-h-min justify-center'>
-                        <img className='aspect-square mix-blend-multiply h-96 min-w-96' src={product.imagen} alt={product.nombre} />
+                <div className='flex-wrap flex items-start  justify-center  shadow-xl border border-gray-300 rounded-xl p-4'>
+                    <section className='flex  max-h-min justify-center object-contain'>
+                        <img className=' mix-blend-multiply h-96 min-w-96 ' src={product.imagen} alt={product.nombre} />
                     </section>
-                    <section className='inline-block'>
+                    <section className='inline-block mx-auto'>
                         <h6 className='text-quinto text-sm'>{product.marca}</h6>
                         <h1 className='text-quinto text-2xl sm:text-3xl font-bold '>{product.nombre}</h1>
                     </section>
