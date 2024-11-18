2660f79e83fb09bbca3a9dbe90ccad12ba2e2dc0
santiago mojica <samojica08@gmail.com>
2024-11-18 14:42:05 -0500

    Correccion colores y border radius del banner

--- Sadimi/src/components/Carrusel/Carrusel.jsx
+++ Sadimi/src/components/Carrusel/Carrusel.jsx
@@ -6,7 +6,7 @@ function Carrusel() {
     return (
         <div className="mx-4 h-56 sm:h-64 xl:h-80 2xl:h-96 my-4">
             <Carousel>
-                <img className="h-full w-full rounded-md sm:rounded-b-[3rem] object-cover object-center " src={Portada} alt="Banner" />
+                <img className="h-full w-full rounded-3xl  object-cover object-center " src={Portada} alt="Banner" />
             </Carousel>
         </div>
     )
