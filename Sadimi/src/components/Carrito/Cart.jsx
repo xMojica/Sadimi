12e3dc2acfa19b7afbf287d5077dedf0f51d0705
santiago mojica <samojica08@gmail.com>
2024-11-28 12:23:14 -0500

    Carrito de compras

--- Sadimi/src/components/Carrito/Cart.jsx
+++ Sadimi/src/components/Carrito/Cart.jsx
@@ -4,6 +4,7 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import { useNavigate } from 'react-router-dom';
 import Trash from './Trash';
+import Atras from "../Login/Header/Atras"
 
 function Cart() {
     const { carrito, setCarrito } = useContext(Context);
@@ -46,38 +47,43 @@ function Cart() {
     return (
         <>
             <Header />
+            <span className='pt-2 flex  pl-4 z-[54]'>
+                <Atras />
+            </span>
             {carrito.length > 0 ? (
                 <main className="flex flex-col items-start justify-start w-full gap-4 p-4 mt-8 md:flex-row">
-
                     <section className="w-full md:w-2/3">
                         {carrito.map((product) => (
                             <article
                                 key={product._id}
-                                className="relative flex flex-wrap items-center p-2 mb-2 border-2 border-gray-300 rounded-lg bg-tercero text-quinto"
-                            >
-                                <div className=''>
+                                className="relative flex flex-col p-2 mb-2 border-2 border-gray-300 rounded-lg sm:flex-row bg-tercero text-quinto">
+                                <div className='flex items-center justify-center h-52'>
                                     <img
+                                        className="h-auto w-44 mix-blend-multiply"
                                         src={product.imagen}
-                                        alt={product.nombre}
-                                        className="h-32 mix-blend-multiply md:h-44 lg:h-52" />
+                                        alt={product.nombre} />
                                 </div>
-                                <div>
-                                    <h1 className="text-sm font-bold lg:text-xl">{product.nombre}</h1>
-                                    <div className="">
-                                        <div className="">
-                                            <button
-                                                className="px-2 rounded-lg"
-                                                onClick={() => manejarCantidad(product, -1)}
-                                            >
-                                                -
-                                            </button>
-                                            <p>{product.cantidad}</p>
-                                            <button
-                                                className="px-2 rounded-lg"
-                                                onClick={() => manejarCantidad(product, 1)}
-                                            >
-                                                +
-                                            </button>
+                                <div className='flex flex-col justify-between w-full mt-4 ml-4'>
+                                    <span className='w-full'>
+                                        <h1 className="text-lg font-bold md:text-2xl lg:text-3xl">{product.nombre}</h1>
+                                        <h2 className='mt-2 text-2xl font-bold text-cuarto'>$ 4.999.900</h2>
+                                    </span>
+                                    <div className="flex flex-row items-center justify-between w-full">
+                                        <div className='items-center flex-flex-col'>
+                                            <div className="flex flex-row items-center justify-between text-2xl w-28 text-quinto gap-x-4">
+                                                <button
+                                                    className="px-2 rounded-lg bg-segundo"
+                                                    onClick={() => manejarCantidad(product, -1)}>
+                                                    -
+                                                </button>
+                                                <p className=''>{product.cantidad}</p>
+                                                <button
+                                                    className="px-2 rounded-lg bg-segundo"
+                                                    onClick={() => manejarCantidad(product, 1)}>
+                                                    +
+                                                </button>
+                                            </div>
+                                            <p className='mt-2 text-sm '>+100 Unidades disponibles</p>
                                         </div>
                                         <Trash eliminar={eliminarProducto} product={product} />
                                     </div>
@@ -101,9 +107,13 @@ function Cart() {
                                 Total: <span className="float-end">${total}</span>
                             </h4>
                         </div>
-                        <button className="p-2 text-xl rounded-xl bg-primero text-tercero hover:scale-105">
-                            Pagar
-                        </button>
+                        <div className='flex gap-4'>
+                            <button className="p-2 px-4 text-xl border-2 border-gray-300 rounded-xl text-quinto hover:scale-105 hover:bg-cuarto hover:text-tercero hover:border-none">Vaciar</button>
+                            <button className="w-full p-2 text-xl rounded-xl bg-primero text-tercero hover:scale-105">
+                                Pagar
+                            </button>
+                        </div>
+
                     </section>
                 </main>
             ) : (
--- Sadimi/src/components/Carrito/Trash.jsx
+++ Sadimi/src/components/Carrito/Trash.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 const Button = (Props) => {
-    return (
-        <StyledWrapper>
-            <button className="delete-button" onClick={() => { Props.eliminar(Props.product) }}>
-                <svg className="delete-svgIcon" viewBox="0 0 448 512">
-                    <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
-                </svg>
-            </button>
-        </StyledWrapper>
-    );
+  return (
+    <StyledWrapper>
+      <button className="mx-4 delete-button" onClick={() => { Props.eliminar(Props.product) }}>
+        <svg className="delete-svgIcon" viewBox="0 0 448 512">
+          <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+        </svg>
+      </button>
+    </StyledWrapper>
+  );
 }
 
 const StyledWrapper = styled.div`
@@ -17,13 +17,12 @@ const StyledWrapper = styled.div`
     width: 40px;
     height: 40px;
     border-radius: 12px;
-    background-color: var(--primero);
-    border: none;
+    background-color: var(--tercero);
+    border: 2px solid #D1D5DB;
     font-weight: 600;
     display: flex;
     align-items: center;
     justify-content: center;
-    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.164);
     cursor: pointer;
     transition-duration: 0.5s;
     overflow: hidden;
@@ -36,14 +35,14 @@ const StyledWrapper = styled.div`
   }
 
   .delete-svgIcon path {
-    fill: var(--tercero);
+    fill: var(--quinto);
   }
 
   .delete-button:hover {
-    padding: 4px;
-    width: 90px;
+    padding: 8px;
+    width: 120px;
     transition-duration: 0.5s;
-    background-color: var(--cuarto);
+    background-color: var(--tercero);
     align-items: center;
   }
 
@@ -61,15 +60,16 @@ const StyledWrapper = styled.div`
   .delete-button::before {
     display: none;
     content: "Eliminar";
-    color: var(--tercero);
+    color: var(--quinto);
     transition-duration: 0.5s;
-    font-size: 2px;
+    font-size: 4px;
+    
   }
 
   .delete-button:hover::before {
     display: block;
     padding-right: 10px;
-    font-size: 13px;
+    font-size: 16px;
     opacity: 1;
     transform: translateY(0px);
     transition-duration: 0.5s;
